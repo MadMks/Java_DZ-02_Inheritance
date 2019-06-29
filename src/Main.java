6e071b447e229f82b71420c42d42b0abb7bda4fc
Makc <mks59k@gmail.com>
2019-06-29 16:16:08 +0300

    Task 01 done

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
-import classes.EnglishTranslator;
+import classes.task01.EnglishTranslator;
 
 public class Main {
 
     public static void main(String[] args) {
 
         // Task 01
-//        EnglishTranslator engTranslator = new EnglishTranslator("English");
-//        System.out.println("Lang: " + engTranslator.getLanguage());
-//        engTranslator.ShowInfo();
+        EnglishTranslator engTranslator = new EnglishTranslator("English");
+        System.out.println("Lang: " + engTranslator.getLanguage());
+        engTranslator.ShowInfo();
 
         // Task 02
 
--- src/classes/task01/Translator.java
+++ src/classes/task01/Translator.java
@@ -1,4 +1,4 @@
-package classes;
+package classes.task01;
 
 public abstract class Translator {
 
